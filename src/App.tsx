21bebb0bff12a0cc4ea34a7a4ea80ec9211c5115
ThomasDev4 <tl.lemallier@gmail.com>
2024-10-28 18:49:05 +0100

    sorry

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import './App.css'
-import NavBar from './components/NavBar/index'
-import Match from './components/Match'
-import PSG from './assets/logo-psg.png'
-import OM from './assets/logo-om.png'
-import Terrain from './assets/terrain-de-foot.png'
-
+import "./App.css";
+import NavBar from "../components/NavBar/index";
+import Match from "../components/Match";
+import PSG from "./assets/logo-psg.png";
+import OM from "./assets/logo-om.png";
+/* import Terrain from "./assets/terrain-de-foot.png";
+ */
 /* const teams = [
     { name: "PSG", logoClub: "" },
 
@@ -14,17 +14,14 @@ import Terrain from './assets/terrain-de-foot.png'
     },
 ]; */
 
-
 function App() {
-   return (
-    <>
-        <NavBar/>
-        <Match logoClub={PSG} name={'PSG'}/>
-        <Match logoClub={OM} name={'OM'}/>
-
-    </>
-   )
-
+	return (
+		<>
+			<NavBar />
+			<Match logoClub={PSG} name={"PSG"} />
+			<Match logoClub={OM} name={"OM"} />
+		</>
+	);
 }
 
-export default App
+export default App;
